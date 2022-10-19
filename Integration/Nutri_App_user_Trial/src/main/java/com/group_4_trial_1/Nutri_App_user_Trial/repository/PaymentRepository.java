14e58040cff245ae598c76b3eb3477dcbb2a1e9c
Tanya997 <92269781+Tanya997@users.noreply.github.com>
2022-10-19 09:48:57 +0530

    Payment Repository updated

--- Integration/Nutri_App_user_Trial/src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/PaymentRepository.java
+++ Integration/Nutri_App_user_Trial/src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/PaymentRepository.java
@@ -1,12 +1,17 @@
 package com.sprint_1.payment_module.repository;
 
-import com.sprint_1.payment_module.dto.PaymentDTO;
+
+import com.sprint_1.payment_module.entity.Payment;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 //import org.springframework.data.jpa.repository.Query;
+
 
 //import java.util.Optional;
 
-public interface PaymentRepository extends JpaRepository<PaymentDTO, Long> {
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
     //@Query("SELECT p FROM Payment p WHERE p.userId = ?1")
     //Optional<PaymentDTO> findPaymentByUserId(String userId);
 }
